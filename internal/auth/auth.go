df0d1dea889eb32aab3a1da5e71a4efc81dddf3b
tbirddv <terracb1997@gmail.com>
2025-08-29 15:55:19 -0400

    fixed auth

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -10,12 +10,12 @@ var ErrNoAuthHeaderIncluded = errors.New("no authorization header included")
 
 // GetAPIKey -
 func GetAPIKey(headers http.Header) (string, error) {
-	authHeader := headers.Get("uthorization")
+	authHeader := headers.Get("Authorization")
 	if authHeader == "" {
 		return "", ErrNoAuthHeaderIncluded
 	}
 	splitAuth := strings.Split(authHeader, " ")
-	if len(splitAuth) < 2 || splitAuth[0] != "piKey" {
+	if len(splitAuth) < 2 || splitAuth[0] != "ApiKey" {
 		return "", errors.New("malformed authorization header")
 	}
 
